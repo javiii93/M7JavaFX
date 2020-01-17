047c245f4476b6ede33b5adb87b4b6ddbc333031
javiii93 <lindejavi93@gmail.com>
2020-01-17 16:57:02 +0100

    first commit

---
+++ src/application/Controller.java
@@ -0,0 +1,16 @@
+package application;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.*;
+public class Controller implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		mostrar();
+	}
+public void mostrar() {
+	System.out.println("hjh");
+}
+}
